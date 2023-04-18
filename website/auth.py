fcc84949cbfd4d28c7a9bfcc6e90d80edea8bfa2
Iago Livino Ferreira <iago.ferreira@five9.com>
2023-04-18 16:58:15 +0100

    first commit v1 / split frondend and backend

--- website/auth.py
+++ website/auth.py
@@ -4,39 +4,45 @@ from flask import request
 from flask import flash
 from flask import redirect
 from flask import url_for
-from flask_login import login_user
-from flask_login import login_required
-from flask_login import logout_user
-from .models import User, JoinRequest
-from .models import Shop
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db, utils
+
+from .user import _User
 from .utils import *
+from .utils import _login_required
 
 auth = Blueprint('auth', __name__)
 
 
+@auth.errorhandler(401)
+def not_authorized(e):
+    flash('user not logged!', category='error')
+    return redirect(url_for('.login'))
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        if current_user.role == 'emp':
+    user = get_current_user()
+    if user.is_authenticated:
+        if user.role == 'emp':
             return redirect(url_for('views.emp_home'))
         else:
             return redirect(url_for('views.home'))
-    success, email, password, user = get_user_data(request, User, Shop)
-    if success:
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                if current_user.role == 'emp':
-                    return redirect(url_for('views.emp_home'))
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again', category='error')
-    if email and password:
-        flash('User/Shop does not exist!', category='error')
-    return render_template("login.html", user=current_user)
+
+    if request.method == 'POST':
+        args = request.url.split('?')[1] if 'shop' in request.args else ''
+        r = requests.post(API_URL + f'token?{args}', data=request.form)
+        if r.status_code == 201:
+            token = json.loads(r.json()).get('access_token')
+            user = _User()
+            user.set_data(requests.get(API_URL + f'user?token={token}').json())
+            user.token = token
+            login_user(user)
+            flash('Logged in successfully!', category='success')
+            if user.role == 'emp':
+                return redirect(url_for('views.emp_home'))
+            return redirect(url_for('views.home'))
+        else:
+            flash(f'Failed to login. Error: {r.json()}', category='error')
+    return render_template("login.html", user=user)
 
 
 @auth.route('/login', methods=['GET', 'POST'], subdomain='employee')
@@ -45,38 +51,63 @@ def emp_login():
 
 
 @auth.route('/logout')
-@login_required
+@_login_required
 def logout():
-    logout_user()
-    return redirect(url_for('.login'))
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+    headers = {'Authorization': f'{user.token}'}
+    r = requests.delete(API_URL + 'token', headers=headers)
+    if r.status_code == 200:
+        logout_user()
+        return redirect(url_for('.login'))
+    flash(f'Could not logout. Server response: {r.json()}', category='error')
+    if user.role == 'emp':
+        return redirect(url_for('views.emp_home'))
+    return redirect(url_for('views.home'))
 
 
 @auth.route('/logout', subdomain='employee')
 @emp_required
 def emp_logout():
-    logout_user()
-    return redirect(url_for('.emp_login'))
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+    headers = {'Authorization': f'{user.token}'}
+    r = requests.delete(API_URL + 'token', headers=headers)
+    if r.status_code == 200:
+        logout_user()
+        return redirect(url_for('.emp_login'))
+    flash(f'Could not logout. Server response: {r.json()}', category='error')
+    return redirect(url_for('views.emp_home'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def join_signup():
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+    headers = {'Authorization': f'{user.token}'}
     shop_name = request.args.get('shop')
     if not shop_name:
         return render_template('notfound.html')
-    shop = Shop.query.filter_by(shop_name=shop_name).first()
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('first_name')
         password = request.form.get('password')
+        r = requests.get(API_URL + f'shop/{shop_name}')
+        if r.status_code == 204:
+            flash('shop does not exist', category='error')
+            return render_template("sign-up.html", user=user)
+        shop_id = r.json().get('id')
 
-        try:
-            user = User.query.filter_by(shop_id=shop.id, email=email).first()
-        except AttributeError as e:
-            flash(f'Shop does not exist!\nPlease add the correct shop in the URL!', category='error')
-            return redirect('/direct-signup?shop=<existent_shop_here>')
-        if user:
-            flash('Email already exists!', category='error')
-        elif len(email) < 4:
+        if len(email) < 4:
             flash('Email must be greater than 3 chars.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 chars.', category='error')
@@ -84,49 +115,38 @@ def join_signup():
             flash('Password must be greater than 6 chars.', category='error')
         else:
             # add join request to database
-            data = '{"first_name":"%s",' \
-                   '"last_name":"%s",' \
-                   '"email":"%s",' \
-                   '"contact":"%s",' \
-                   '"password":"%s",' \
-                   '"role":"%s"}' \
-                   % (request.form.get('first_name'), request.form.get('last_name'), request.form.get('email'),
-                      request.form.get('contact'), request.form.get('password'), request.form.get('role')
-                      )
-            new_join_request = JoinRequest(
-                shop_id=shop.id,
-                data=data
-            )
-            db.session.add(new_join_request)
-            db.session.commit()
-            flash('Join request sent!', category='success')
+            data = {"first_name": f"{request.form.get('first_name')}",
+                    "last_name": f"{request.form.get('last_name')}",
+                    "email": f"{request.form.get('email')}",
+                    "contact": f"{request.form.get('contact')}",
+                    "password": f"{request.form.get('password')}",
+                    "role": f"{request.form.get('role')}",
+                    "shop_id": shop_id}
+            r = requests.put(API_URL + 'create-join-request', headers=headers, data=data)
+            if r.status_code == 201:
+                flash('Join request sent!', category='success')
             return redirect(url_for('.login') + f'?shop={shop_name}')
-
-    return render_template("sign-up.html", user=current_user)
+    return render_template("sign-up.html", user=user)
 
 
 # test method. signup endpoint should send a request to existing
 @auth.route('/direct-signup', methods=['GET', 'POST'])
 @admin_required
 def direct_signup():
-    if request.method == 'POST':
-        data = request.form
-        shop = Shop.query.filter_by(id=Shop.query.filter_by(shop_name=data.get('shop')).first().id).first()
-        email = data.get('email')
-        first_name = data.get('first_name')
-        password = data.get('password')
-        contact = data.get('contact')
-        last_name = data.get('last_name')
-        role = data.get('role')
+    # put together
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+    # all this
 
-        try:
-            user = User.query.filter_by(shop_id=shop.id, email=email).first()
-        except AttributeError as e:
-            flash(f'Shop does not exist!\nPlease add the correct shop in the URL!', category='error')
-            return redirect('/direct-signup?shop=<existent_shop_here>')
-        if user:
-            flash('Email already exists!', category='error')
-        elif len(email) < 4:
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password = request.form.get('password')
+
+        if len(email) < 4:
             flash('Email must be greater than 4 chars.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 2 chars.', category='error')
@@ -134,73 +154,80 @@ def direct_signup():
             flash('Password must be greater than 7 chars.', category='error')
         else:
             # add user to database
-            utils.create_user(data)
-            flash('Account created!', category='success')
-
+            if 'u' in request.args:
+                r = requests.patch(API_URL + f'user/{request.args.get("u")}', data=request.form)
+            else:
+                r = requests.put(API_URL + 'user', data=request.form)
+            if r.status_code != 201:
+                flash(f'Could not create user! Server error: {r.json()}', category='success')
             return redirect(url_for('views.users'))
 
-    return render_template("direct-sign-up.html", user=current_user)
+    if 'u' in request.args:
+        r = requests.get(API_URL + f'user/{request.args.get("u")}')
+        if r.status_code == 200:
+            r2 = requests.get(API_URL + f'shop/{r.json().get("shop_id")}')
+            data = r.json()
+            data['shop_name'] = r2.json().get('shop_name')
+            return render_template("direct-sign-up.html", user=user, prefill=data)
+
+    return render_template("direct-sign-up.html", user=user, prefill=None)
 
 
 @auth.route('/create-employee', methods=['GET', 'POST'])
 @mgr_required
 def create_emp():
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('first_name')
         password = request.form.get('password')
-        contact = request.form.get('contact')
-        last_name = request.form.get('last_name')
-        if current_user.role == 'admin':
-            shop_id = Shop.query.filter_by(shop_name=request.form.get('shop_name')).first().id
-        else:
-            shop_id = current_user.shop_id
-
-        try:
-            user = User.query.filter_by(shop_id=current_user.shop_id, email=email, role='emp').first()
-        except AttributeError as e:
-            flash(f'Shop does not exist!\nPlease add the correct shop in the URL!', category='error')
-            return redirect('/direct-signup?shop=<existent_shop_here>')
-        if user:
-            flash('Email already exists!', category='error')
-        elif len(email) < 4:
+        if len(email) < 4:
             flash('Email must be greater than 4 chars.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 2 chars.', category='error')
         elif len(password) < 7:
             flash('Password must be greater than 7 chars.', category='error')
         else:
-            # add user to database
-            new_user = User(
-                email=email,
-                contact=contact,
-                first_name=first_name,
-                last_name=last_name,
-                password=generate_password_hash(password, method='sha256'),
-                role='emp',
-                shop_id=shop_id
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Employee added!', category='success')
-            return redirect(url_for('views.employees'))
+            data = request.form.to_dict()
+            data['role'] = 'emp'
+            r = requests.put(API_URL + 'employees', data=data)
+            if r.status_code == 201:
+                flash('Employee added!', category='success')
+                return redirect(url_for('views.employees'))
+            flash(f'could not add employee! Server error: {r.json()}', category='success')
 
-    return render_template("create-employee.html", user=current_user)
+    if 'e' in request.args:
+        r = requests.get(API_URL + f'employee/{request.args.get("e")}')
+        if r.status_code == 200:
+            r2 = requests.get(API_URL + f'shop/{r.json().get("shop_id")}')
+            data = r.json()
+            data['shop_name'] = r2.json().get('shop_name')
+            return render_template("create-employee.html", user=user, prefill=data)
+
+    return render_template("create-employee.html", user=user, prefill=None)
 
 
 @auth.route('/create-shop', methods=['GET', 'POST'])
 @admin_required
 def create_shop():
+    user = get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        logout_user()
+        return redirect(url_for('auth.login'))
+
     if request.method == 'POST':
         shop_name = request.form.get('shop_name')
         email = request.form.get('email')
         contact = request.form.get('contact')
         address = request.form.get('address')
 
-        shop = Shop.query.filter_by(shop_name=shop_name).first()
-        if shop:
-            flash('Shop already exists!', category='error')
-        elif len(shop_name) < 4:
+        if len(shop_name) < 4:
             flash('Shop name must be greater than 4 chars.', category='error')
         elif len(email) < 5:
             flash('Email must be greater than 5 chars.', category='error')
@@ -209,15 +236,19 @@ def create_shop():
         elif len(address) < 5:
             flash('Address must be greater than 5 chars.', category='error')
         else:
-            new_shop = Shop(
-                shop_name=shop_name,
-                email=email,
-                contact=contact,
-                address=address
-            )
-            db.session.add(new_shop)
-            db.session.commit()
-            flash('Shop created!', category='success')
-            return redirect(url_for('views.shops'))
+            if 's' in request.args:
+                r = requests.patch(API_URL + f'shop/{request.args.get("s")}', data=request.form)
+            else:
+                r = requests.put(API_URL + 'shops', data=request.form)
+            if r.status_code == 201:
+                flash('Shop created!', category='success')
+                return redirect(url_for('views.shops'))
+            flash(f'could not create shop. Server error: {r.json()}', category='success')
 
-    return render_template('create-shop.html', user=current_user)
+    if 's' in request.args:
+        r = requests.get(API_URL + f'shop/{request.args.get("s")}')
+        if r.status_code == 200:
+            data = r.json()
+            return render_template("create-shop.html", user=user, prefill=data)
+
+    return render_template('create-shop.html', user=user, prefill=None)
--- website/views.py
+++ website/views.py
@@ -1,110 +1,183 @@
 import json
-
 import requests
-from flask import Blueprint, redirect, url_for, request, flash
+from flask import Blueprint, redirect, url_for, flash
 from flask import render_template
-from flask_login import current_user
-from sqlalchemy import or_
-
-from .models import JoinRequest, Shop
-
-from .models import User
 from .utils import emp_required, admin_required, read_required, mgr_required
-from . import db, auth, utils
+from . import utils, API_URL
 
 views = Blueprint('views', __name__)
+
+
+@views.errorhandler(401)
+def not_authorized(e):
+    flash('user not logged!', category='error')
+    return redirect(url_for('auth.login'))
 
 
 @views.route('/')
 @read_required
 def home():
-    return render_template("home.html", user=current_user)
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    return render_template("home.html", user=user)
 
 
 @views.route('/', subdomain='employee')
 @emp_required
 def emp_home():
-    return render_template("employee.html", user=current_user)
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    return render_template("employee.html", user=user)
 
 
 @views.route('/employees')
 @read_required
 def employees():
-    if current_user.role == 'admin':
-        emps = User.query.filter_by(role='emp').all()
+    emps = {}
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    if user.role == 'admin':
+        r = requests.get(API_URL + 'employees')
     else:
-        emps = User.query.filter_by(shop_id=current_user.shop_id, role='emp').all()
-    return render_template("employees.html", user=current_user, employees=emps)
-
-
-@views.route('/delete-employee/<id>')
-@mgr_required
-def delete_emp(id):
-    user = User.query.filter_by(id=id).first()
-    db.session.delete(user)
-    db.session.commit()
-    return redirect(url_for('.employees'))
+        r = requests.get(API_URL + f'employees/shop/{user.shop_id}')
+    if r.status_code == 200:
+        emps = r.json()
+    return render_template("employees.html", user=user, employees=emps)
 
 
 @views.route('/join-requests')
 @mgr_required
 def join_requests():
-    if current_user.role == 'admin':
-        jrs = JoinRequest.query.all()
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    jrs = {}
+    if user.role == 'admin':
+        r = requests.get(API_URL + 'join-requests')
     else:
-        jrs = JoinRequest.query.filter_by(shop_id=current_user.shop_id).all()
-    return render_template('join-requests.html', user=current_user, join_requests=jrs, json=json)
+        r = requests.get(API_URL + f'join-requests/shop/{user.shop_id}')
+    if r.status_code != 204:
+        jrs = r.json()
+    return render_template('join-requests.html', user=user, join_requests=jrs, json=json)
 
 
 @views.route('/shops')
 @admin_required
 def shops():
-    shops = Shop.query.all()
-    return render_template('shops.html', user=current_user, shops=shops)
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    s = requests.get(API_URL + 'shops').json()
+    return render_template('shops.html', user=user, shops=s)
+
+
+@views.route('/users')
+@mgr_required
+def users():
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    _users = {}
+    if user.role == 'admin':
+        r = requests.get(API_URL + 'users')
+    else:
+        r = requests.get(API_URL + f'users/shop/{user.shop_id}')
+    if r.status_code == 200:
+        _users = r.json()
+    return render_template('users.html', user=user, users=_users)
 
 
 @views.route('/accept-join/<id>')
 @mgr_required
 def accept_join(id):
-    try:
-        jr = JoinRequest.query.filter_by(id=id).first()
-    except:
-        flash('something went wrong loading request!', category='error')
-        return redirect(url_for('.join_requests'))
-    if jr.status:
-        flash('join request already processed!', category='error')
-        return redirect(url_for('.join_requests'))
-    data = json.loads(jr.data)
-    utils.create_user(data, shop=Shop.query.filter_by(id=jr.shop_id).first())
-    if User.query.filter_by(email=data.get('email')).first():
-        jr.processed_by = current_user.email
-        jr.status = 'approved'
-        db.session.commit()
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    r = requests.patch(API_URL + f'join-request/{id}', data={"email": user.email, "action": "accept"})
+    if r.status_code == 200:
+        flash('join request processed', category='success')
+    else:
+        flash(f'could not process join request. Server error: {r.json()}', category='error')
     return redirect(url_for('.join_requests'))
 
 
 @views.route('/decline-join/<id>')
 @mgr_required
 def decline_join(id):
-    try:
-        jr = JoinRequest.query.filter_by(id=id).first()
-    except:
-        flash('something went wrong loading request!', category='error')
-        return redirect(url_for('.join_requests'))
-    if jr.status:
-        flash('join request already processed!', category='error')
-        return redirect(url_for('.join_requests'))
-    jr.processed_by = current_user.email
-    jr.status = 'declined'
-    db.session.commit()
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    r = requests.patch(API_URL + f'join-request/{id}', data={"email": user.email, "action": "decline"})
+    if r.status_code == 200:
+        flash('join request processed', category='success')
+    else:
+        flash(f'could not process join request. Server error: {r.json()}', category='error')
     return redirect(url_for('.join_requests'))
 
 
-@views.route('/users')
+@views.route('/remove-employee/<id>')
 @mgr_required
-def users():
-    if current_user.role == 'admin':
-        _users = User.query.all()
+def delete_emp(id):
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    r = requests.delete(API_URL + f'employee/{id}')
+    if r.status_code == 201:
+        flash('employee deleted', category='success')
     else:
-        _users = User.query.filter((User.shop_id == current_user.shop_id) | (or_(User.role == 'mgr', User.role == 'read'))).all()
-    return render_template('users.html', user=current_user, users=_users)
+        flash(f'could not delete employee. Server error: {r.json()}', category='error')
+    return redirect(url_for('.employees'))
+
+
+@views.route('/remove-user/<id>')
+@mgr_required
+def delete_user(id):
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    r = requests.delete(API_URL + f'user/{id}')
+    if r.status_code == 201:
+        flash('user deleted', category='success')
+    else:
+        flash(f'could not delete user. Server error: {r.json()}', category='error')
+    return redirect(url_for('.users'))
+
+
+@views.route('/remove-shop/<id>')
+@admin_required
+def delete_shop(id):
+    user = utils.get_current_user()
+    if not user.is_authenticated:
+        flash('session expired')
+        utils.logout_user()
+        return redirect(url_for('auth.login'))
+    r = requests.delete(API_URL + f'shop/{id}')
+    if r.status_code == 201:
+        flash('shop deleted', category='success')
+    else:
+        flash(f'could not delete shop. Server error: {r.json()}', category='error')
+    return redirect(url_for('.shops'))
+
